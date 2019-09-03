a3152f855d6e170773a4f3087feccd7aff034920
Kevin Li <hackeverything@foxmail.com>
2019-09-03 22:18:41 +0800

    0.0.1版本上传

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # BuzzChat-Server
 BuzzChat Server是一款基于Node.js的开源聊天室服务器，BuzzChat Client是其客户端。
+
+## 程序简介
+本程序主体是基于Javascript编写。因为初学了Photoshop、Node.js以及HTML，想巩固一下自己的能力，于是写出了Buzz Chat。
+软件作者是北邮大一新生Kevin Li，Twitter主页是[@ChinaKevinLi](https://twitter.com/ChinaKevinLi "@ChinaKevinLi")。
+
+## 使用说明
+从GitHub下载源码后，你需要在电脑上安装Node.js，并支持npm。首先需要安装依赖，切换到源码根目录下运行：
+```bash
+npm install
+```
+此时会安装所需要的依赖，然后只需要运行：
+```bash
+node server.js
+```
+软件有以下参数：
+- -p, --chatport [port] 即为Socket.io监听端口
+- -P, --panelport [port] 即为Web管理监听端口，访问localhost:port/[Panel Tocken]即可进入控制面板
+- -n, --chatroomname [chatroomname] 聊天室的名字，会作为客户端的窗口标题
+软件运行后会输出以下参数：
+- Panel Tocken，即进入控制面板需要跟上的参数
+- Chat Tocken，即客户端输入的密匙
+
+## 开源协议
+本程序完全按照Apache 2.0协议开源，基于本程序开发请遵循协议，并标明我的名字和所属学校。
---
+++ server.js
@@ -0,0 +1,74 @@
+//初始化库
+const express = require('express');
+const webpanel = express();
+const commander = require('commander');
+const crypto = require('crypto');
+const stringrandom = require('string-random');
+const chatserver = require('http').createServer();
+const chatio = require('socket.io')(chatserver,{
+    path: '/',
+    serveClient: false
+});
+
+//声明状态
+var panelport = 3000;
+var chatport = 3001;
+var usercount = 0;
+var msgdata = 0;
+var paneltoken = stringrandom(32);
+var chattoken = stringrandom(32);
+var users = [];
+var chatroomname = ""
+var encipher = crypto.createCipheriv('aes-256-ecb', chattoken, null);
+var decipher = crypto.createDecipheriv('aes-256-ecb', chattoken, null);
+
+//初始化交互
+console.log('[*] Panel Tocken:' + paneltoken);
+console.log('[*] Chat Tocken:' + chattoken);
+commander
+    .option('-p, --chatport [port]','The port that chat service listens on.')
+    .option('-P, --panelport [port]','The port that panel listens on.')
+    .option('-n, --chatroomname [chatroomname]','The name of this chatroom.')
+    .parse(process.argv);
+
+if (commander.chatport) chatport = commander.chatport;
+if (commander.panelport) panelport = commander.panelport;
+if (commander.chatroomname) chatroomname = commander.chatroomname;
+
+//控制面板网页
+webpanel.get('/' + paneltoken,function (req, res) {
+  var webcontent = '<title>聊天室控制面板</title>' + '当前在线人数：' + usercount + '</br>' + '聊天数据总量：' + msgdata + '字节'  + '</br></br>用户列表：' + '<ul>'
+  users.forEach(n => webcontent += '<li>' + n + '</li>');
+  webcontent += '</ul>'
+  res.send(webcontent);
+});
+webpanel.listen(panelport, () => console.log('[*] Webpanel listening on port ' + panelport +'.'));
+
+//聊天服务
+chatserver.listen(chatport, () => console.log('[*] Chat service listening on port ' + chatport + '.'));
+chatio.on('connection', function(socket){
+    usercount += 1;
+    console.log('[+] A user connected, ' + usercount + ' users in total now.');
+
+    socket.on('join', function (name){
+      encipher = crypto.createCipheriv('aes-256-ecb', chattoken, null);
+      decipher = crypto.createDecipheriv('aes-256-ecb', chattoken, null);
+      encipher.update('ohayou','utf8','base64');
+      checkkey_flag = encipher.final('base64');
+      chatio.emit('checkkey', checkkey_flag);
+      users[usercount - 1] = name;
+      chatio.emit('join', name);
+    });
+
+    chatio.emit('chatroomname', chatroomname);
+
+    socket.on('message', function (msg){
+      msgdata += msg.length;
+      chatio.emit('message', msg);
+    });
+
+    socket.on('disconnecting', function (reason) {
+      usercount -= 1;
+      console.log('[-] A user left, ' + usercount + ' users in total now.');
+  });
+});
